7d3b913f6410b95db1a3fc5c1dcc77bcb01bf40b
Abdul Muyeed <abdulmuyeed47@gmail.com>
2025-09-01 13:26:38 +0600

    server updated

--- server/configs/connectDB.js
+++ server/configs/connectDB.js
@@ -1,35 +1,20 @@
 // ...existing code...
 import mongoose from "mongoose";
 
-let cached = global.__mongoose; 
-if (!cached) {
-  cached = global.__mongoose = { conn: null, promise: null };
-}
+let mongodb_instance = null;
 
 export const connectDB = async () => {
-  if (cached.conn) {
-    return cached.conn;
+  if (mongodb_instance) {
+    return mongodb_instance;
   }
 
-  const uri = process.env.MONGODB_URI;
-  if (!uri) {
-    throw new Error("MONGODB_URI environment variable is not set");
+  try{
+      mongodb_instance = await mongoose.connect(process.env.MONGODB_URI, { serverSelectionTimeoutMS: 5000 })
+      console.log(`MongoDB connected: ${mongodb_instance.connection.host}`);
+  }catch(err){
+      mongodb_instance = null;
+      console.error(`MongoDB connection error: ${err.message}`);
   }
 
-  if (!cached.promise) {
-    // short serverSelectionTimeoutMS to fail fast in serverless environments
-    cached.promise = mongoose
-      .connect(uri, { serverSelectionTimeoutMS: 5000 })
-      .then((m) => {
-        cached.conn = m;
-        console.log(`MongoDB connected: ${m.connection.host}`);
-        return m;
-      })
-      .catch((err) => {
-        cached.promise = null;
-        throw err;
-      });
-  }
-
-  return cached.promise;
+  return mongodb_instance;
 };
